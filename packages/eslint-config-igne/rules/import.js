baebc385d7a17e65949cccd990b2e19d8507abec
Jorn Luiten <jorn@igne.nl>
2024-07-02 09:07:11 +0200

    fix: import rules were not properly exported

--- packages/eslint-config-igne/rules/import.js
+++ packages/eslint-config-igne/rules/import.js
@@ -1,5 +1,6 @@
-module.export = {
-  'import/no-anonymous-default-export': 'warn',
-  'import/newline-after-import': 'error',
-  'import/no-duplicates': 'error',
-}
+module.exports = {
+  "import/no-anonymous-default-export": "warn",
+  "import/first": "error",
+  "import/newline-after-import": "error",
+  "import/no-duplicates": "error",
+};
