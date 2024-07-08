3fb22e7bea58da3d477f20a620cd24f60511a236
Jorn Luiten <jorn@igne.nl>
2024-07-08 17:18:55 +0200

    feat(eslint): add expo config

---
+++ packages/eslint-config-igne/expo.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ["./index.js"],
+  plugins: ["expo"],
+  ignorePatterns: [
+    // JS files can end up in build intermediates, eg:
+    // android/app/build/intermediates/assets/debug/EXDevMenuApp.android.js
+    "android/app/build",
+  ],
+  rules: {
+    "expo/no-env-var-destructuring": ["error"],
+    "expo/no-dynamic-env-var": ["error"],
+  },
+};
