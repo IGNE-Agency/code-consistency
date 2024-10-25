b3c53900a75272cfab56f28242334f61d56208e2
Jorn Luiten <jorn@igne.nl>
2024-10-25 13:14:27 +0200

    implement flat config with eslint 9

---
+++ packages/eslint-config-igne/configs/generic.js
@@ -0,0 +1,14 @@
+export const configGeneric = [
+  {
+    name: "igne-agency/generic",
+    rules: {
+      // Allow console- and debugger statements in development with warning, but throw errors in production build
+      // `console.warn` and `console.error` are always allowed
+      "no-console":
+        process.env.NODE_ENV === "production"
+          ? ["error", { allow: ["warn", "error"] }]
+          : ["warn", { allow: ["warn", "error"] }],
+      "no-debugger": process.env.NODE_ENV === "production" ? "error" : "warn",
+    },
+  },
+];
---
+++ packages/eslint-config-igne/configs/import.js
@@ -0,0 +1,49 @@
+import importPlugin from "eslint-plugin-import";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+
+const typeScriptExtensions = [".ts", ".tsx"];
+const allExtensions = [...typeScriptExtensions, ".js", ".jsx", ".mjs", ".cjs"];
+
+// importPlugin for correctness and simpleImportSort for.. sorting.
+export const configImport = [
+  importPlugin.flatConfigs.recommended,
+
+  {
+    name: "igne-agency/import",
+    settings: {
+      "import/extensions": allExtensions,
+      "import/parsers": {
+        "@typescript-eslint/parser": typeScriptExtensions,
+      },
+      "import/resolver": {
+        node: {
+          extensions: allExtensions,
+        },
+        typescript: {
+          alwaysTryTypes: true,
+          project: ["tsconfig.json", "tsconfig.*.json"],
+        },
+      },
+    },
+    rules: {
+      // TypeScript compilation already ensures that named imports exist in the referenced module
+      "import/named": "off",
+
+      "import/no-anonymous-default-export": "warn",
+      "import/first": "error",
+      "import/newline-after-import": "error",
+      "import/no-duplicates": "error",
+    },
+  },
+
+  {
+    name: "igne-agency/import-sorting",
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+];
