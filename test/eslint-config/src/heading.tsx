e084f23846c363428b1533664668c953e64304c6
Jorn Luiten <jorn@igne.nl>
2024-10-26 11:53:36 +0200

    test: add test project for eslint config

---
+++ test/eslint-config/src/heading.tsx
@@ -0,0 +1,24 @@
+// Should have no errors for alias
+import { Hi } from "@/hi";
+
+type AllHeadingProps = React.ComponentPropsWithoutRef<"h1"> & {
+  semanticLevel: 1 | 2 | 3 | 4 | 5 | 6;
+  size?: 1 | 2 | 3 | 4 | 5 | 6;
+};
+
+export const Heading = ({
+  semanticLevel,
+  size,
+  className,
+  children,
+  ...props
+}: AllHeadingProps) => {
+  const Hx: keyof JSX.IntrinsicElements = `h${semanticLevel}`;
+  const headingSize = `heading-${size || semanticLevel}`;
+
+  return (
+    <Hx className={`${headingSize} ${className}`} {...props}>
+      <Hi /> {children}
+    </Hx>
+  );
+};
